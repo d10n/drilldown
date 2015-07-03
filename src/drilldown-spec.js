9f82c432d546af2aec6688a4c54deee89ef453ac
d10n <david@bitinvert.com>
2015-07-03 13:04:02 -0400

    Add .set() which creates missing parent objects

--- src/drilldown-spec.js
+++ src/drilldown-spec.js
@@ -60,6 +60,34 @@ describe('drilldown', function() {
         expect(example).to.eql(original);
         expect(newResult).to.be.undefined();
     });
+    it('should set the value of a deeply-nested property', function() {
+        var originalValue = example.quux.ping.pong;
+        expect(dd(example)('quux')('ping')('pong').val).to.equal(originalValue);
+        var newValue = false;
+        var newResult = dd(example)('quux')('ping')('pong').set(newValue);
+        expect(newResult).to.equal(newValue);
+        expect(example.quux.ping.pong).to.equal(newValue);
+    });
+    it('should set the value of a nonexistent property', function() {
+        var expected = _.cloneDeep(example);
+        expected.quux.abc = {xyz: 5};
+        var newResult = dd(example)('quux')('abc')('xyz').set(5);
+        expect(example).to.eql(expected);
+        expect(newResult).to.equal(5);
+    });
+    it('should not set a value without a context', function() {
+        var original = _.cloneDeep(example);
+        var newExample = dd(example).set(5);
+        expect(example).to.eql(original);
+        expect(newExample).to.be.undefined();
+    });
+    it('should set a deeply-nested value on an empty object', function() {
+        var myObject = {};
+        var newExample = dd(myObject)('req')('query')('page').set(5);
+        var expected = {req: {query: {page: 5}}};
+        expect(myObject).to.eql(expected);
+        expect(newExample).to.equal(5);
+    });
     it('should indicate whether a property exists', function() {
         expect(dd(example)('foo').exists).to.be.true();
         expect(dd(example)('oof').exists).to.be.false();
@@ -70,6 +98,11 @@ describe('drilldown', function() {
         expect(idLocal.val).to.equal(originalIdLocal);
         expect(idLocal.update(originalIdLocal + 1)).to.equal(originalIdLocal + 1);
         expect(idLocal('subId').update(originalIdLocal + 2)).to.be.undefined();
+        var newSubId = idLocal('subId').set(originalIdLocal + 3);
+        expect(newSubId).to.equal(originalIdLocal + 3);
+        expect(example['[id.local]']).to.eql({
+            subId: originalIdLocal + 3
+        });
     });
     it('should not drill into own properties', function() {
         expect(dd(example)('__proto__').exists).to.be.false();
--- src/drilldown.js
+++ src/drilldown.js
@@ -63,9 +63,13 @@ var isFunction = (function() {
  * @param {object} object
  * @param _context
  * @param _key
+ * @param _root
+ * @param _rootPath
  * @returns {Function}
  */
-function dd(object, _context, _key) {
+function dd(object, _context, _key, _root, _rootPath) {
+    _root = _root || object;
+    _rootPath = _rootPath || [];
     var drill = function(key) {
         var nextObject = (
             object &&
@@ -73,10 +77,35 @@ function dd(object, _context, _key) {
             object[key] ||
             undefined
         );
-        return dd(nextObject, object, key);
+        return dd(nextObject, object, key, _root, _rootPath.concat(key));
     };
     drill.val = object;
     drill.exists = object !== undefined;
+    drill.set = function(value) {
+        if (_rootPath.length === 0) {
+            return;
+        }
+        var contextIterator = _root;
+        for (var depth = 0; depth < _rootPath.length; depth++) {
+            var key = _rootPath[depth];
+            var isFinalDepth = (depth === _rootPath.length - 1);
+            if (!isFinalDepth) {
+                contextIterator[key] = (
+                    contextIterator.hasOwnProperty(key) &&
+                    typeof contextIterator[key] === 'object' ?
+                        contextIterator[key] : {}
+                );
+                contextIterator = contextIterator[key];
+            } else {
+                _context = contextIterator;
+                _key = key;
+            }
+        }
+        _context[_key] = value;
+        drill.val = value;
+        drill.exists = value !== undefined;
+        return value;
+    };
     drill.update = function(value) {
         if (drill.exists) {
             _context[_key] = value;
